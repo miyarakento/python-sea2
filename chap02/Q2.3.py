00fb12b31903cd50b1447ed71921a7e642a0548d
miyarakento <n23031@std.it-college.ac.jp>
2023-07-05 07:54:14 +0000

    Q2-4 END

---
+++ chap02/Q2.3.py
@@ -0,0 +1,12 @@
+print(0.6)
+print(2e2)
+print(3.2e3)
+
+print(1.0 == 0.5 + 0.5)
+print(0.3 == 0.1 + 0.1 + 0.1)
+
+print(12 / 55)
+print(120 // 55)
+print(divmod(120, 55))
+print(divmod(100, 55))
+print(divmod(450, 55))
---
+++ chap02/Q2_2.py
@@ -0,0 +1,9 @@
+a = 10
+print(a * 2)
+a = a * 2
+print(a)
+a *= 2
+print(a)
+my_data = 10
+MYDAta = 10
+a = 10
