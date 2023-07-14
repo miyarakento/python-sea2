c0cede1856f86da51113c74c476b1dd52e4f5345
miyarakento <n23031@std.it-college.ac.jp>
2023-07-14 03:16:35 +0000

    3-5

---
+++ chap03/Q3_5_1.py
@@ -0,0 +1,12 @@
+a = 1
+while a < 10:
+    if a % 2 == 0:
+        print(a)
+        a += 1
+
+while True:
+    if a % 2 == 0:
+        print(a)
+        a += 1
+        if a >= 10:
+            break
